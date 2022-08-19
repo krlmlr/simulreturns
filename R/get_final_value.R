08bf58599ba0902d26f55558e0b6b2bfb582d7a4
LayalChristine <layal.lettry@gmail.com>
2022-08-19 21:30:03 +0200

    Add new variable "exp_return_pct" and define final values types with case_when

--- R/get_final_value.R
+++ R/get_final_value.R
@@ -15,7 +15,7 @@
 #' Otherwise, ordinary annuities (1st annuity on the 31st of December).
 #'
 #' @return
-#' - `FINAL_VALUE`: Investment final values time series.
+#' - `FINAL_VALUE`: Investment final value time serie.
 #'
 #' @examples
 #' # Final value of immediate annuities with first capital and yearly expected of
@@ -24,6 +24,7 @@
 #'   n_per = 30,
 #'   v_0 = 5000,
 #'   exp_return = 10,
+#'   compound = TRUE,
 #'   x_yearly = 12000,
 #'   immediate = TRUE
 #' )
@@ -35,15 +36,31 @@
 get_final_value <- function(n_per,
                             v_0,
                             exp_return,
+                            compound,
                             x_yearly = 0,
                             immediate = TRUE) {
 
-  # Simple interest
+  # Expected return in percent
+  exp_return_pct <- exp_return / 100
+
+  # Final value
   FINAL_VALUE <- tibble(
-    period = 1:n_per,
-    smpl_int_v0 = v_0 * (1 + period * (exp_return / 100)),
-    comp_int_v0 = v_0 * (1 + (exp_return / 100)^period),
-  )
+    period = 1:n_per
+  ) %>%
+    mutate(
+      final_value = case_when(
+        # Simple return, no annuities
+        x_yearly == 0 &
+          compound == FALSE ~ v_0 * (1 + period * exp_return_pct),
+        # Compound return, no annuities
+        x_yearly == 0 &
+          compound == TRUE ~ v_0 * (1 + exp_return_pct)^period,
+        # Simple return, with annuities
+        x_yearly > 0 &
+          compound == FALSE ~ v_0 * (1 + period * exp_return_pct) +
+          x_yearly * ((1 + exp_return_pct)^period - 1) / exp_return_pct
+      )
+    )
 
   # Output
   return(FINAL_VALUE)
