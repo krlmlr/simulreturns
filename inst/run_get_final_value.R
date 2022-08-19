cac8d944592558cfddaeeb4a44433d859dbc74cc
LayalChristine <layal.lettry@gmail.com>
2022-08-19 21:30:26 +0200

    Update run file for various final values types

--- inst/run_get_final_value.R
+++ inst/run_get_final_value.R
@@ -4,10 +4,32 @@ library(simulreturns)
 # For local test or development purposes load sources virtually:
 devtools::load_all(".")
 
-FINAL_VALUE <- get_final_value(
+# Simple return, no annuities
+FINAL_VALUE_SMPL_NOANNUIT <- get_final_value(
   n_per = 30,
   v_0 = 5000,
   exp_return = 10,
+  compound = FALSE,
+  x_yearly = 0,
+  immediate = TRUE
+)
+
+# Compound return, no annuities
+FINAL_VALUE_CMP_NOANNUIT <- get_final_value(
+  n_per = 30,
+  v_0 = 5000,
+  exp_return = 10,
+  compound = TRUE,
+  x_yearly = 0,
+  immediate = TRUE
+)
+
+# Simple return, with annuities
+FINAL_VALUE_SMPL_ANNUIT <- get_final_value(
+  n_per = 30,
+  v_0 = 5000,
+  exp_return = 10,
+  compound = FALSE,
   x_yearly = 12000,
   immediate = TRUE
 )
