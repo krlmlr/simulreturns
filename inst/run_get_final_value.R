5a651324833f14dd3dbddbbd762389f593ad0098
LayalChristine <layal.lettry@gmail.com>
2022-08-19 21:00:45 +0200

    Create run file
    Run get_final_value() function

---
+++ inst/run_get_final_value.R
@@ -0,0 +1,13 @@
+# Load from repository
+library(simulreturns)
+
+# For local test or development purposes load sources virtually:
+devtools::load_all(".")
+
+get_final_value(
+n_per = 30,
+v_0 = 5000,
+exp_return = 10,
+x_yearly = 12000,
+immediate = TRUE
+)
